139a5bb6063fee18bfa5394b89a38c5dcdb1f21a
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2024-12-09 10:51:02 -0300

    Login endpoint

--- src/app.py
+++ src/app.py
@@ -22,6 +22,14 @@ Migrate(app, db)
 jwt = JWTManager(app)
 CORS(app)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify({ "error": "page not found"}), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return jsonify({ "error": "internal server error"}), 500
+
 app.register_blueprint(api, url_prefix="/api")
 
 if __name__ == '__main__':
--- src/routes.py
+++ src/routes.py
@@ -1,5 +1,5 @@
 import datetime
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, abort
 from flask_jwt_extended import create_access_token
 from werkzeug.security import generate_password_hash, check_password_hash
 from models import User
@@ -8,32 +8,59 @@ api = Blueprint("api", __name__)
 
 @api.route('/login', methods=['POST'])
 def login():
-    pass
+    try:
+        username = request.json.get('username')
+        password = request.json.get('password')
+
+        if not username:
+            return jsonify({ "error": "Field (username) is requred"}), 400
+        if not password:
+            return jsonify({ "error": "Field (username) is requred"}), 400
+        
+        user = User.query.filter_by(username=username).first()
+
+        if not user:
+            return jsonify({ "error": "Username/Password are incorrects, please try again"}), 401
+        
+        if not check_password_hash(user.password, password):
+            return jsonify({ "error": "Username/Password are incorrects, please try again"}), 401
+        
+        expire = datetime.timedelta(days=3)
+        access_token = create_access_token(identity=str(user.id), expires_delta=expire)
+
+        return jsonify({ "acceses_token": access_token}), 200
+
+    except Exception as e:
+        abort(500)
 
 @api.route('/register', methods=['POST'])
 def register():
-    
-    username = request.json.get('username')
-    password = request.json.get('password')
+    try:
 
-    if not username:
-        return jsonify({ "error": "this field (username) is requred"}), 400
-    if not password:
-        return jsonify({ "error": "this field (username) is requred"}), 400
-    
-    found = User.query.filter_by(username=username).first()
+        username = request.json.get('username')
+        password = request.json.get('password')
 
-    if found:
-        return jsonify({ "error": "this field (username) already taken"}), 400
-    
-    user = User()
-    user.username = username
-    user.password = generate_password_hash(password)
-    user.save()
-    
-    expire = datetime.timedelta(days=3)
-    access_token = create_access_token(identity=str(user.id), expires_delta=expire)
+        if not username:
+            return jsonify({ "error": "this field (username) is requred"}), 400
+        if not password:
+            return jsonify({ "error": "this field (username) is requred"}), 400
+        
+        found = User.query.filter_by(username=username).first()
 
-    return jsonify({ "acceses_token": access_token}), 200
+        if found:
+            return jsonify({ "error": "this field (username) already taken"}), 400
+        
+        user = User()
+        user.username = username
+        user.password = generate_password_hash(password)
+        user.save()
+        
+        expire = datetime.timedelta(days=3)
+        access_token = create_access_token(identity=str(user.id), expires_delta=expire)
+
+        return jsonify({ "acceses_token": access_token}), 200
+    
+    except Exception as e:
+        abort(500)
 
     
